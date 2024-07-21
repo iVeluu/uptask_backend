f0303af57ee12eeb5293c676d7005a6171dc2d23
iVeluu <velualex10@gmail.com>
2024-07-20 23:50:52 -0600

    add a middleware that delete all the notes asociate to one task

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
+import Note from "./Note";
 
 const taskStatus = {
     PENDING: 'pending',
@@ -63,6 +64,14 @@ export const TaskSchema = new Schema({
         }
     ]
 }, {timestamps: true})
+
+//Middleware (para borrar las notas relacionas a la tarea)
+
+TaskSchema.pre('deleteOne', {document: true}, async function() {
+    const taskId = this._id
+    if(!taskId) return
+    await Note.deleteMany({task: taskId})
+})
 
 const Task = mongoose.model<ITask>('Task', TaskSchema)
 export default Task
